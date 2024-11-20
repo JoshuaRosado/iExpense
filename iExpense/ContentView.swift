d507909316c386523fc6b2e25e034178de315e4a
JoshuaRosado <joshua06rosado@gmail.com>
2024-11-19 19:28:57 -0600

    update

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,16 +7,100 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+
+struct ExpenseItem: Identifiable, Codable {
+
+    
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem](){
+        didSet {
+            // ENCODE the Objects created into JSON
+            if let encoded = try? JSONEncoder().encode(items) {
+                // and Save it using UserDefaults.
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
     }
+     
+    init() { // New Initializer
+        
+        // if savedItems from UserDefaults
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            // Copy the code from savedItems
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                // The new Object is now The returning Object saved from UserDefault as DECODED
+                items = decodedItems
+                return
+            }
+        }
+        // if not return an empty array
+        
+        items = []
+    }
+}
+
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                
+                
+                ForEach(expenses.items, id: \.id){ item in
+                    
+                    // Display item name and item type on .leading side
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            
+                            Text(item.type)
+                        }
+                        // spacer in between the two objects Horizontally
+                        Spacer()
+                        
+                        // Display item amount on .trailing side
+                        Text(item.amount, format:.currency(code:"USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                EditButton()
+                Button("Add Expense", systemImage: "plus"){
+                
+                
+                    
+                    showingAddExpense = true
+                    
+                    
+                    
+                }
+            }
+            .sheet(isPresented: $showingAddExpense){
+                AddView(expenses: expenses)
+            }
+            
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
